d9a7cfd8af3340b7546a820d244adb9d31580f3b
Drew <andrewtennant2012@gmail.com>
2023-06-11 15:41:39 -0500

    update index

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
-<?php
-
-echo "Hello world";
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container">
+    <h1>PHP + MySQL CRUD Demo</h1>
+    <p>Create, read, update, and delete records below</p>
+</div>
+</body>
+</html>
